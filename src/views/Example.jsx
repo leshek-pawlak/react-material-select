e9b58716c240eeca0b6a5361c7719b58f250597d
Mikhail S. Pobolovets <styx.mp@gmail.com>
2017-09-27 17:33:59 +0300

    Compatibility with React 16

--- src/views/Example.jsx
+++ src/views/Example.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import Highlight from 'react-highlight'
 import ReactMaterialSelect from 'components/ReactMaterialSelect'
 
